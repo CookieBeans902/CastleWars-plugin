3bd486bd015e612c59dbc18d382e22b2ad1444d8
Sarath Chandra <sarathj2006@gmail.com>
2026-05-11 17:25:24 +0530

    feat: Added suggestions, permissions for kit loading

--- src/main/java/me/emumaps/commands/KitSystem.java
+++ src/main/java/me/emumaps/commands/KitSystem.java
@@ -12,57 +12,70 @@ import org.bukkit.inventory.ItemStack;
 
 public class KitSystem {
     KitManager kitManager;
+
+    static final String KIT_NAME = "Kit Name";
+
     public KitSystem(KitManager kitManager, Commands registrar) {
         this.kitManager = kitManager;
         LiteralArgumentBuilder<CommandSourceStack> kit;
         kit = createCommand();
         registrar.register(kit.build(), "Kit commands");
     }
-    LiteralArgumentBuilder<CommandSourceStack> createCommand() {
+
+    public LiteralArgumentBuilder<CommandSourceStack> createCommand() {
         LiteralArgumentBuilder<CommandSourceStack> kit;
         kit = Commands.literal("cwkit");
         kit.then(Commands.literal("save")
-            .then(Commands.argument("Kit Name", StringArgumentType.string())
-                            .executes(ctx -> {
-                                    Entity entity = ctx.getSource().getExecutor();
-                                    if (entity == null) return 0;
-                                    String kitName = ctx.getArgument("Kit Name", String.class);
-                                    if (!(entity instanceof Player player)) {
-                                        return 0;
-                                    }
-                                    if (player.hasPermission("castlewars.kit.save")) {
-                                        player.sendMessage("You have saved the kit " + kitName);
-                                        this.kitManager.addKit(kitName,player.getInventory().getContents());
-                                    }
-                                    else {
-                                        player.sendMessage("You do not have permission to save kits");
-                                        return 0;
-                                    }
-                                    return Command.SINGLE_SUCCESS;
-                                }
-                                )
-            ))
-            .then(Commands.literal("load")
-                .then(Commands.argument("Kit Name", StringArgumentType.string())
-                        .executes(ctx -> {
+                .requires(ctx -> ctx.getSender().hasPermission("castlewars.kit.save"))
+                .then(Commands.argument(KIT_NAME, StringArgumentType.string())
+                    .executes(ctx -> {
                             Entity entity = ctx.getSource().getExecutor();
-                            if(entity == null) return 0;
-                            String kitName = ctx.getArgument("Kit Name", String.class);
-                            if(entity instanceof Player player) {
+                            String kitName = ctx.getArgument(KIT_NAME, String.class);
+                            if (!(entity instanceof Player player)) {
+                                return 0;
+                            }
+                            player.sendMessage("You have saved the kit " + kitName);
+                            this.kitManager.addKit(kitName, player.getInventory().getContents());
+                            return Command.SINGLE_SUCCESS;
+                        }
+                    )
+                )
+            )
+            .then(Commands.literal("load")
+                .then(Commands.argument(KIT_NAME, StringArgumentType.string())
+
+                    .requires(ctx -> ctx.getSender().hasPermission("castlewars.kit.load"))
+                    .suggests( (ctx,builder) -> {
+                            String[] kitNames= kitManager.getKitNames();
+                            String prefix = builder.getRemainingLowerCase();
+                            for(String kitName:kitNames) {
+                                if(!kitName.toLowerCase().startsWith(prefix)) continue;
+                                builder.suggest(kitName);
+                            }
+                            return builder.buildFuture();
+                        }
+                    )
+                    .executes(ctx -> {
+                            Entity entity = ctx.getSource().getExecutor();
+                            if (entity == null) return 0;
+                            String kitName = ctx.getArgument(KIT_NAME, String.class);
+                            if (entity instanceof Player player) {
                                 ItemStack[] items = this.kitManager.loadKit(kitName);
-                                if(items == null) {
+                                if (items == null) {
                                     player.sendMessage("That kit does not exist");
                                     return 0;
                                 }
                                 player.sendMessage("You have loaded the kit " + kitName);
                                 player.getInventory().setContents(items);
+                                return Command.SINGLE_SUCCESS;
+                            } else {
+                                entity.sendMessage("You do not have permission to load kits");
                                 return 0;
                             }
-                            else {
-                                return 0;
-                            }
-                        })
-        ));
+                        }
+                    )
+                )
+            );
         return kit;
     }
 }
--- src/main/java/me/emumaps/managers/KitManager.java
+++ src/main/java/me/emumaps/managers/KitManager.java
@@ -5,17 +5,24 @@ import org.bukkit.inventory.ItemStack;
 import java.util.Map;
 
 public class KitManager {
-    Map<String,byte[]> kits;
+    Map<String, byte[]> kits;
+
     public KitManager() {
         kits = new java.util.HashMap<>();
     }
+
+    public String[] getKitNames() {
+        return kits.keySet().toArray(new String[0]);
+    }
+
     public ItemStack[] loadKit(String kitName) {
         byte[] inv = kits.get(kitName);
-        ItemStack[] decodedInv = ItemStack.deserializeItemsFromBytes(inv);
-        return decodedInv;
+        if (inv == null) return new ItemStack[0];
+        return ItemStack.deserializeItemsFromBytes(inv);
     }
+
     public void addKit(String kitName, ItemStack[] inventory) {
         byte[] encodedInv = ItemStack.serializeItemsAsBytes(inventory);
-        kits.put(kitName,encodedInv);
+        kits.put(kitName, encodedInv);
     }
 }
